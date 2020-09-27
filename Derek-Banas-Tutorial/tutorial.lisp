9da3a496f48d7f660f6aa6b3b0e21bf5b32197a1
Marc Partensky <marc.partensky@gmail.com>
2020-09-27 23:11:17 +0200

    hello world

--- Derek-Banas-Tutorial/tutorial.lisp
+++ Derek-Banas-Tutorial/tutorial.lisp
@@ -1,4 +1,31 @@
+#!/usr/bin/env clisp
+;;; everything is a list inside of lisp
 ;;;; comment
 ;;; basic comment
 ;; comment indented
-(format t "hello ~%") ; comment after a line of code
+;(format t "hello ~%") ; comment after a line of code
+
+;(defvar *name* (read))
+
+;(defun hello-you (name)
+  ;(format t "Hello ~a! ~%" name)
+;)
+
+(setq *print-case* :capitalize)
+;(hello-you *name*)
+
+
+;;; ~a : shows the value
+;;; ~s : shows quots around the value
+;;; ~10a : Adds 10 spaces for the value with extra space
+;;; ~10@a : Adds 10 spaces for the value with extra space
+
+;(+ 5 4) = 9
+;(+ 5 (-6 2)) = 9
+
+;[+] [5] [4] [nil]
+
+
+(format t "number with commas ~:d" 1000000)
+
+;(format t "10 dollars ~$ ~% 10")
